1314740922c3e8c160ba0b08e99f9b12091b0c3a
Quentin Vannarath <qvannarath@gmail.com>
2023-07-08 17:44:22 +0200

    🐛 [DOCUMENT] Fixed Document doesn't have a <title> element

--- pages/_document.js
+++ pages/_document.js
@@ -40,13 +40,13 @@ export default class MyDocument extends Document {
           <meta name='apple-mobile-web-app-status-bar-style' content='default' />
           <meta name='apple-mobile-web-app-title' content={APP_NAME} />
           <meta name='description' content={APP_DESCRIPTION} />
-          <meta name='title' content={APP_TITLE} />
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
           <meta name='theme-color' content='#FFFFFF' />
           <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
           <link rel='manifest' href='/manifest.json' />
           <link rel='shortcut icon' href='/favicon.ico' />
+          <title>{APP_TITLE}</title>
         </Head>
         <body>
           <Main />
